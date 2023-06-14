db9ad2b8674325e5943664d553ff41f83f14689e
Joseph <JPMeehan5@Gmail.com>
2023-06-14 12:43:25 -0700

    More Migration updates

--- tools/migrateFiles.py
+++ tools/migrateFiles.py
@@ -12,9 +12,31 @@ packList = os.listdir(origin)
 
 def flattenSource(source):
     if type(source) == str:
-        return source
+        return source if source != "" else "A5E Adventurer's Guide"
     else:
         return source["name"]
+
+def abbr(word):
+    match word:
+        case "abjuration":
+            return "abj"
+        case "conjuration":
+            return "conj"
+        case "divination":
+            return "div"
+        case "enchantment":
+            return "enc"
+        case "evocation":
+            return "evo"
+        case "illusion":
+            return "ill"
+        case "necromancy":
+            return "nec"
+        case "transmutation":
+            return "trs"
+
+def migrateAction(action, o5e) -> bool:
+    return True
 
 def migrateMonster(system: dict) -> dict:
     o5e = {
@@ -160,7 +182,8 @@ def migrateObject(system: dict) -> dict:
             "ep": 0,
             "gp": 0,
             "pp": 0
-        }
+        },
+        "type": "backpack"
     }
     equipment = {
         "templates": ["activatedEffect", "action", "mountable"],
@@ -176,14 +199,16 @@ def migrateObject(system: dict) -> dict:
         },
         "strength": None,
         "stealth": False,
-        "proficient": True
+        "proficient": True,
+        "type": "equipment"
     }
     consumable = {
         "templates": ["activatedEffect", "action"],
         "consumableType": "potion",
         "uses": {
             "autoDestroy": False
-        }
+        },
+        "type": "consumable"
     }
     tool = {
         "toolType": "",
@@ -191,15 +216,41 @@ def migrateObject(system: dict) -> dict:
         "ability": "int",
         "chatFlavor": "",
         "proficient": 0,
-        "bonus": ""
+        "bonus": "",
+        "type": "tool"
     }
     weapon = {
         "templates": ["activatedEffect", "action", "mountable"],
         "weaponType": "simpleM",
         "baseItem": "",
         "properties": {},
-        "proficient": True
+        "proficient": True,
+        "type": "weapon"
     }
+    match system["objectType"]:
+        case "ammunition":
+            o5e.update(consumable)
+        case "armor":
+            o5e.update(equipment)
+        case "clothing":
+            o5e.update(equipment)
+        case "consumable":
+            o5e.update(consumable)
+        case "container":
+            o5e.update(backpack)
+        case "jewelry":
+            o5e.update(equipment)
+        case "miscellaneous":
+            o5e.update(equipment)
+        case "shield":
+            o5e.update(equipment)
+        case "tool":
+            o5e.update(tool)
+        case "weapon":
+            o5e.update(weapon)
+        case _:
+            o5e["type"] = "loot"
+    
     return o5e
 
 """ SPELL
@@ -227,12 +278,36 @@ def migrateObject(system: dict) -> dict:
 # Turns the spell into a spell
 def migrateSpell(system: dict) -> dict:
     o5e = {
+        "templates": ["activatedEffect", "action"],
         "description": {
             "value": system["description"],
             "chat": "",
             "unidentified": ""
         },
-        "source": flattenSource(system["source"])
+        "source": flattenSource(system["source"]),
+        "level": system["level"],
+        "school": abbr(system["schools"]["primary"]),
+        "components": {
+            "vocal": system["components"]["vocalized"],
+            "somatic": system["components"]["seen"],
+            "material": system["components"]["material"],
+            "ritual": system["ritual"],
+            "concentration": system["concentration"]
+        },
+        "materials": {
+            "value": system["materials"],
+            "consumed": system["materialsConsumed"],
+            "cost": 0,
+            "supply": 0
+        },
+        "preparation": {
+            "mode": "prepared",
+            "prepared": False
+        },
+        "scaling": {
+            "mode": "none",
+            "formula": None
+        }
     }
     return o5e
 
@@ -325,23 +400,29 @@ def migrateDestiny(system: dict) -> dict:
 for p in packList:
     with open(os.path.join(origin,p), "r") as read_file:
         data = json.load(read_file)
-        match data["type"]:
+        match data["type"]: # a5e type
             case "npc":
                 data["system"] = migrateMonster(data["system"])
             case "feature":
                 data["system"] = migrateFeature(data["system"])
+                data["type"] = data["system"]["type"]
+                data["system"].pop("type")
             case "maneuver":
                 data["system"] = migrateManeuver(data["system"])
             case "object":
                 data["system"] = migrateObject(data["system"])
+                data["type"] = data["system"]["type"]
+                data["system"].pop("type")
             case "spell":
                 data["system"] = migrateSpell(data["system"])
             case "background":
                 data["system"] = migrateBackground(data["system"])
             case "culture":
                 data["system"] = migrateCulture(data["system"])
+                data["type"] = "background"
             case "destiny":
                 data["system"] = migrateDestiny(data["system"])
+                data["type"] = "background"
     print(data)
     break
     # with open(os.path.join(packPath,p), "r") as writeFile:
