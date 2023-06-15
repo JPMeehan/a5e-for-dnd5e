7cfb5e78b0195ab39c92a172acbe5c82815fa69b
Joseph <JPMeehan5@Gmail.com>
2023-06-15 14:42:12 -0700

    Spell Folders

--- tools/buildReferenceFolder.py
+++ tools/buildReferenceFolder.py
@@ -2,21 +2,26 @@ import os
 import json
 import sys
 
-targetPack = sys.argv[1]
-packPath = os.path.join(".\src", "packs", targetPack)
-packList = os.listdir(packPath)
 
-# folderStruct = {"_id": {"name": "foobar", "parent": "_id"}}
-folderStruct = {}
+def build(targetPack):
+    packPath = os.path.join(".\src", "packs", targetPack)
+    packList = os.listdir(packPath)
 
-for p in packList:
-    # print(p)
-    with open(os.path.join(packPath,p), "r") as read_file:
-        data = json.load(read_file)
-        if 'type' not in data:
-            continue
-        if (data["type"] == "Item"):
-            folderStruct[data["_id"]] = {"name": data["name"], "parent": data["folder"]}
+    # folderStruct = {"_id": {"name": "foobar", "parent": "_id"}}
+    folderStruct = {}
 
-with open(os.path.join(".", "tools", "referenceFolders", targetPack + ".json"), "w") as write_file:
-    write_file.write(json.dumps(folderStruct, indent=2))
+    for p in packList:
+        # print(p)
+        with open(os.path.join(packPath,p), "r") as read_file:
+            data = json.load(read_file)
+            if 'type' not in data:
+                continue
+            if (data["type"] == "Item"):
+                folderStruct[data["_id"]] = {"name": data["name"], "parent": data["folder"]}
+
+    with open(os.path.join(".", "tools", "referenceFolders", targetPack + ".json"), "w") as write_file:
+        write_file.write(json.dumps(folderStruct, indent=2))
+
+if len(sys.argv) > 1:
+    t = sys.argv[1]
+    build(t)
--- tools/spellFolders.py
+++ tools/spellFolders.py
@@ -1,23 +1,23 @@
 import os
 import json
-import sys
 import utils
+import buildReferenceFolder
 
-targetPack = sys.argv[1]
+targetPack = "spells"
 packPath = os.path.join(".\src", "packs", targetPack)
 packList = os.listdir(packPath)
 
 class Folder:
-    def __init__(self, name: str, color: str, parentFolder: str) -> None:
-        name = name
-        color = color
-        flags = {}
-        sort = 0
-        sorting = 'a'
-        _type = 'item'
-        folder = parentFolder
-        _id = utils.randomID()
-        _key = '!folders!' + _id
+    def __init__(self, name: str, color: str | None = None, parentFolder: str | None = None) -> None:
+        self.name = name
+        self.color = color
+        self.flags = {}
+        self.sort = 0
+        self.sorting = 'a'
+        self._type = 'item'
+        self.folder = parentFolder
+        self._id = utils.randomID()
+        self._key = '!folders!' + self._id
         
     def __repr__(self) -> str:
         return repr(vars(self))
@@ -30,6 +30,24 @@ class encoder(json.JSONEncoder):
             d.pop("_type")
         return d
 
-for p in packList:
-    with open(os.path.join(packPath,p), "r") as read_file:
-        data = json.load(read_file)
+outerFolders = ["Spells", "Rare Spells"]
+innerFolders = ["Cantrip", "1st level", "2nd level", "3rd level", "4th level", "5th level", "6th level", "7th level", "8th level", "9th level"]
+
+for o in outerFolders:
+    outer = Folder(o)
+    print(outer)
+    filename = outer.name.replace(' ', '_') + '_' + outer._id + ".json"
+    with open(os.path.join(packPath,filename), "w") as writeFile:
+        writeFile.write(json.dumps(outer, indent=2, cls=encoder))
+    for i in innerFolders:
+        inner = Folder(i, parentFolder=outer._id)
+        print(inner)
+        filename = inner.name.replace(' ', '_') + '_' + inner._id + ".json"
+        with open(os.path.join(packPath,filename), "w") as writeFile:
+            writeFile.write(json.dumps(inner, indent=2, cls=encoder))
+
+buildReferenceFolder.build(targetPack)
+
+# for p in packList:
+#     with open(os.path.join(packPath,p), "r") as read_file:
+#         data = json.load(read_file)
