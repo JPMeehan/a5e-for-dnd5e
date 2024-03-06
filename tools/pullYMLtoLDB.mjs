3039796b00a58a1a43d46cda3f4fe1b589b73554
Joseph <JPMeehan5@Gmail.com>
2024-03-06 14:36:57 -0800

    Small build process update

--- tools/pullYMLtoLDB.mjs
+++ tools/pullYMLtoLDB.mjs
@@ -1,15 +1,16 @@
-import { compilePack } from "@foundryvtt/foundryvtt-cli";
-import { promises as fs } from "fs";
-import path from "path";
+import { compilePack } from '@foundryvtt/foundryvtt-cli';
+import { promises as fs } from 'fs';
 
-const packs = await fs.readdir(path.join("src", "packs"));
+const MODULE_ID = process.cwd();
+const yaml = true;
+
+const packs = await fs.readdir('./src/packs');
 for (const pack of packs) {
-  if (pack === ".gitattributes") continue;
-  console.log("Packing " + pack);
-  const src = path.join("src", "packs", pack);
-  const dest = path.join("packs", pack);
-  await compilePack(src, dest, {
-    yaml: true,
-    log: true,
-  });
+  if (pack === '.gitattributes') continue;
+  console.log('Packing ' + pack);
+  await compilePack(
+    `${MODULE_ID}/src/packs/${pack}`,
+    `${MODULE_ID}/packs/${pack}`,
+    { yaml }
+  );
 }
