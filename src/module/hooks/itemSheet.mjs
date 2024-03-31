99760ab8cd3eb23f6973223a16e1609ccdb7d0e3
Joseph <JPMeehan5@Gmail.com>
2024-03-30 22:55:43 -0700

    Rare spell input

--- src/module/hooks/itemSheet.mjs
+++ src/module/hooks/itemSheet.mjs
@@ -8,19 +8,22 @@ import { moduleID, modulePath } from '../utils.mjs';
  */
 export async function spells(sheet, html, context) {
   const spell = sheet.item;
-  console.log(spell, html);
-  const rare = spell.getFlag(moduleID, 'rareSpell') ?? false;
+  const editable = context.editable;
+  const details = html.find('.tab.details');
+
+  // Secondary Schools
   const secondarySchools = spell.getFlag(moduleID, 'secondarySchools') ?? [];
-  const rareInput = await renderTemplate(
-    modulePath + 'templates/legacy/rareSpell-partial.hbs',
-    { rare }
-  );
   // TODO: Transform secondarySchools
   //   const secondaryInput = await renderTemplate(
   //     modulePath + 'templates/legacy/secondarySchools-partial.hbs',
   //     { secondarySchools }
   //   );
-  const details = html.find('.tab.details');
-  const school = details.find('.form-group:nth-child(3)');
-  school.after();
+
+  // Rare Spells
+  const rare = spell.getFlag(moduleID, 'rareSpell') ?? false;
+  const rareInput = await renderTemplate(
+    modulePath + 'templates/legacy/rareSpell-partial.hbs',
+    { rare, editable }
+  );
+  details.find('.form-group:nth-child(6)').after(rareInput);
 }
