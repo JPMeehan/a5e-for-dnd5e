81016941c8bc6dac8213a1d970b1b59f59a8df4d
Joseph <JPMeehan5@Gmail.com>
2024-08-07 09:13:09 -0700

    Fixed display of compendium browser tabs

--- src/module/hooks/compendiumBrowser.mjs
+++ src/module/hooks/compendiumBrowser.mjs
@@ -7,25 +7,26 @@ export function addTabs() {
     const tabs = dnd5e.applications.CompendiumBrowser.TABS;
     const cultureTab = {
         tab: "cultures",
-        label: "TYPES.Item.culturePl",
+        label: "TYPES.Item.a5e-for-dnd5e.culturePl",
         svg: modulePath + 'assets/icons/subtypes/culture.svg',
         documentClass: "Item",
         types: [moduleTypes.culture]
     }
+    tabs.splice(4, 0, cultureTab);
     const destinyTab = {
         tab: "destinies",
-        label: "TYPES.Item.destinyPl",
+        label: "TYPES.Item.a5e-for-dnd5e.destinyPl",
         svg: modulePath + 'assets/icons/subtypes/destiny.svg',
         documentClass: "Item",
         types: [moduleTypes.destiny]
     }
+    tabs.splice(6, 0, destinyTab);
     const maneuverTab = {
         tab: "maneuvers",
-        label: "TYPES.Item.maneuverPl",
+        label: "TYPES.Item.a5e-for-dnd5e.maneuverPl",
         svg: modulePath + 'assets/icons/subtypes/maneuver.svg',
         documentClass: "Item",
         types: [moduleTypes.culture]
     }
-
-    tabs.push(cultureTab, destinyTab, maneuverTab);
+    tabs.splice(9, 0, maneuverTab);
 }
